df7dc2533eefdefbdca7e87d6f97ce4449ea17df
Ronak Karia <ronak@sozodesign.co.uk>
2025-08-13 22:57:45 +0530

    why csc section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import CommunitySection from "@/components/community";
 import CTA from "@/components/cta";
 import HeroSection from "@/components/hero";
 import Products from "@/components/products";
+import SkipTheHassle from "@/components/skip-the-hassle";
 import Stats from "@/components/stats";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <Stats />
       <Products />
+      <SkipTheHassle />
       <CTA />
       <CommunitySection />
     </>
