b2fe18867d19218a7454f171de6bcd9619de271b
Ronak Karia <ronak@sozodesign.co.uk>
2025-08-19 22:45:36 +0530

    mission values

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import AboutIntro from "@/components/about-intro";
+import MissionValues from "@/components/mission-values";
 
 export default function About() {
   return (
     <>
       <AboutIntro />
+      <MissionValues />
     </>
   );
 }
