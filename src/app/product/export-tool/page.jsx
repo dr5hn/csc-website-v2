21697fd1f53f9521183d752d2817693e0e5ae1b4
Ronak Karia <ronak@sozodesign.co.uk>
2025-09-03 00:14:02 +0530

    export tool wip

--- src/app/product/export-tool/page.jsx
+++ src/app/product/export-tool/page.jsx
@@ -1,5 +1,8 @@
+import CtaExportTool from "@/components/product/export-tool/cta";
+import ExportPreview from "@/components/product/export-tool/export-formats";
 import Features from "@/components/product/export-tool/features";
 import HeroExportTool from "@/components/product/export-tool/hero";
+import UseCases from "@/components/product/export-tool/usecases";
 import WhyChoose from "@/components/product/export-tool/why-choose";
 
 export default function Page() {
@@ -8,6 +11,9 @@ export default function Page() {
       <HeroExportTool />
       <WhyChoose />
       <Features />
+      <ExportPreview />
+      <UseCases />
+      <CtaExportTool />
     </>
   );
 }
